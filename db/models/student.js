c6f3d47c1f0e1596798f97f38a9679522b37dfb8
yillachen <chn.yilla@gmail.com>
2022-01-22 14:22:51 -0500

    fix: comments in /models/test.js

--- db/models/student.js
+++ db/models/student.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define('student', {
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false,
+const Student = db.define('student', { // 1. Create a Student Model for your database, it takes two args: instance name and the attributes/fields you want to include for each instance. We're using "student" as the instance in this case.
+  firstName: { // 2. The test specs require you have 3 attributes to pass: firstName, lastName and email
+    type: Sequelize.STRING, // Every attribute/field has to have a TYPE, we're using STRING to indicate it can include letters, numbers and symbols.
+    allowNull: false // Here we're saying it cannot be an empty/null field
   },
   lastName: {
     type: Sequelize.STRING,
@@ -13,13 +13,13 @@ const Student = db.define('student', {
   email: {
     type: Sequelize.STRING,
     allowNull: false,
-    validate: {
-      isEmail: true,
+    validate: { // Here we create a special validation that makes sure the "email" field is an email (under the hood, Sequelize checks if it includes an @ symbol and a .com/.org/etc.)
+      isEmail: true, // We keep "validate:" in an object, because we add more than one constraint, in this case, we're using only one constraint.
     },
   },
 });
 
-Student.beforeCreate((student) => {
+Student.beforeCreate((student) => { // Here we're introducing yet another validation checker that goes through your instance properties BEFORE you're allowed to add it into the database. In this lifecycle event/hook, you're making sure the firstName and lastName are entered with a capitalized first letter.
   const nameFirst = student.firstName;
   const nameLast = student.lastName;
 
@@ -27,4 +27,4 @@ Student.beforeCreate((student) => {
   student.lastName = nameLast[0].toUpperCase() + nameLast.slice(1);
 });
 
-module.exports = Student;
+module.exports = Student; // we export the model to be used in the rest of the repo
--- db/models/test.js
+++ db/models/test.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define('test', {
+const Test = db.define('test', { // Similar set up to the Student model
   subject: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -13,6 +13,6 @@ const Test = db.define('test', {
   },
 });
 
-Test.belongsTo(Student, { as: 'student' });
+Test.belongsTo(Student, { as: 'student' }); // So now that you have two models, what do you even do with the information?? Let's make them "talk" to each other. This association tells us that unique test can belong to only one student. The 'as' allows you to name that association field in your table whatever you want. When this populates in your database, you'll see "studentId" at the end of the Test table for each instance.
 
 module.exports = Test;
